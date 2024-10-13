fc1e21ce551ca9b2f556c46f43b61c1c416749e3
Drake Damon <dddamon06@gmail.com>
2024-10-13 13:49:11 -0400

    completed sequence lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]  # Start with the first two numbers
+    for i in range(2, length):
+        next_value = fibonacci[-1] + fibonacci[-2]  # Sum of the last two numbers
+        fibonacci.append(next_value)
+    print(fibonacci[:length])  # Print up to the specified length
+
+print_fibonacci(9)
+# Output: [0, 1, 1, 2, 3, 5, 8, 13, 21]
